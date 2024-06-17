d7e38791f00e71decd057de09153bc17b0cb0e14
birando <birger1@live.com>
2024-06-17 14:25:56 +0200

    fileSetup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./assets/styles/style.css";
+import Task from "./Task.js";
+
+const task1 = new Task(
+  "Eat",
+  "Making food and eating it",
+  "15.06.24",
+  "HIGH",
+  false,
+  "Basic"
+);
+
+console.log(task1);
