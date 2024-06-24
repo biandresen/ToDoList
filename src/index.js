50e3be3217c39d5606f39bf0a5f2e6f0860a2d66
birando <birger1@live.com>
2024-06-24 14:23:06 +0200

    feat: add get elements
    Refs: #3 #4 #10

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+//IMPORTS
 import "../src/assets/styles/style.css";
+////////////////////////////////////////////
+
+//GET ELEMENTS
+const menuButton = document.querySelector("[data-menu-button]");
+const filtersButton = document.querySelector("[data-filters-button]");
+const allTasksButton = document.querySelector("[data-all-tasks-button]");
+const todayButton = document.querySelector("[data-today-button]");
+const tomorrowButton = document.querySelector("[data-tomorrow-button]");
+const weekButton = document.querySelector("[data-week-button]");
+const monthButton = document.querySelector("[data-month-button]");
+const projectsButton = document.querySelector("[data-projects-button]");
+const newTaskButton = document.querySelector("[data-new-task-button]");
+const newTaskSubmitArea = document.querySelector("[data-new-task-submit-area]");
+/////////////////////////////////////////////////////////////////////////////////////////
+//DECLARE LISTS
+const taskList = [];
+const projectList = [];
+////////////////////////
